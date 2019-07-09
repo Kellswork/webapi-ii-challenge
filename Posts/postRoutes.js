467028199ea65525ab4852a23c6a733b0e06bdce
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-09 15:12:12 +0100

    implement all get endpoints

--- Posts/postRoutes.js
+++ Posts/postRoutes.js
@@ -1,7 +1,59 @@
 const express = require('express');
-
+const db = require('../data/db');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const posts = await db.find(req.params);
+    if (posts.length > 0) {
+      return res.status(200).json(posts);
+    }
+    res.status(404).json({ error: 'No post has been created yet' });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
 
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.id);
+
+    if (!post.length) {
+      return res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+    res.status(200).json(post);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The posts information could not be retrieved.' });
+  }
+});
+
+router.get('/:postId/comments', async (req, res) => {
+  try {
+    const post = await db.findById(req.params.postId);
+
+    if (!post.length) {
+      return res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+    const comments = await db.findPostComments(post[0].id);
+    if (!comments.length) {
+      return res
+        .status(404)
+        .json({ message: 'There are no comments for this post' });
+    }
+    res.status(200).json(comments);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: 'The comments information could not be retrieved.' });
+  }
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const posts = require('./Posts/postRoutes');
 const server = express();
 
 server.get('/', (req, res) => {
     res.status(200).json('Welcome to Server-side Routing with Node JS')
 })
+
+server.use('/api/posts', posts)
 
 server.listen(4100, ()=> console.log('Server listening on port 4100'))
